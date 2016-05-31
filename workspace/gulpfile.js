ca088fb90ea82eac188a8f5459a117b63841719c
Chito Cheng <cheng@chito.hk>
2016-05-31 21:49:33 +0800

    i18n added

--- workspace/gulpfile.js
+++ workspace/gulpfile.js
@@ -1,9 +1,12 @@
+'use strict';
+
 var path = require('path');
 var gulp = require('gulp');
 var assemble = require('assemble');
+var i18n = require('gulp-html-i18n');
 var app = assemble();
 
-app.data({site: {title: 'HKA'}});
+app.data({site: {title: 'Test'}});
 app.option('layout', 'default.hbs');
 
 gulp.task('load', function(cb) {
@@ -19,8 +22,22 @@ gulp.task('assemble', ['load'], function() {
     .pipe(app.dest(function(file) {
       file.extname = '.html';
       file.path = file.basename;
-      return 'dist';
+      return 'dist/raw';
     }));
 });
 
-gulp.task('default', ['assemble']);
+gulp.task('i18n', ['assemble'], function(){
+  var dest  = './dist';
+  var index = './dist/raw/*.html';
+
+  return gulp.src(index)
+    .pipe(i18n({
+      langDir: './src/i18n',
+      trace: false,
+      createLangDirs: true,
+      langRegExp: /\[\[ ?([\w-.]+) ?\]\]/g
+    }))
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('default', ['i18n']);
